ff330c10bcde927c8e27ae66433cca7905cc908f
Russ <32114457+RustyDotson@users.noreply.github.com>
2026-03-03 21:40:33 +0900

    Created systems for movements and display, updated main to test out player sprite.
    Currently experiencing errors.

---
+++ src/ecs/Systems.hpp
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+
+void movementSystem(Registry& registry, float deltaTime) {
+    registry.ForEach<Transform, Velocity>(
+        [&] (Entity entity, Transform& transform, Velocity& velocity) {
+            transform.x += velocity.vx * deltaTime;
+            transform.y += velocity.vy * deltaTime;
+        }
+    );
+}
+
+void inputSystem(Registry& registry) {
+
+    float const speed = 200.0f; //pixels per second
+
+    registry.ForEach<Transform, Velocity>(
+        [&] (Entity entity, Transform& transform, Velocity& velocity) {
+
+            if (!registry.hasComponent<PlayerTag>(entity) || !registry.hasComponent<Input>(entity)) {
+                return;
+            }
+
+            Input& input = registry.getComponent<Input>(entity);
+
+            velocity.vx = 0;
+            velocity.vy = 0;
+
+            // Handle input for the entity
+            if (input.up) {
+                velocity.vy -= speed;
+            }
+            if (input.down) {
+                velocity.vy += speed;
+            }
+            if (input.left) {
+                velocity.vx -= speed;
+            }
+            if (input.right) {
+                velocity.vx += speed;
+            }
+
+        }
+    );
+}
+
+void renderSystem(Registry& registry, sf::Window& window){
+    registry.ForEach<Sprite>(
+        [&] (Entity entity, Sprite& sprite) {
+            window.draw(sprite.shape);
+        }
+    );
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "ecs/Entity.hpp"
 #include "ecs/Registry.hpp"
 #include "ecs/Components.hpp"
+#include "ecs/Systems.hpp"
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 
 
@@ -22,15 +24,36 @@ int main(){
 
 
 
-    std::cout << "Created entity: " << player << std::endl;
-    registry.printRegistry();
-    registry.destroy(player);
-    std::cout << "Destroyed entity: " << player << std::endl;
-    registry.printRegistry();
+    //std::cout << "Created entity: " << player << std::endl;
+    //registry.printRegistry();
+    //registry.destroy(player);
+    //std::cout << "Destroyed entity: " << player << std::endl;
+    //registry.printRegistry();
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {  
+        float dt = clock.restart().asSeconds();
+
+        registry.ForEach<Input> (
+            [&] (Entity entity, Input& input) {
+                // This is just for testing, it will make the entity move in a circle
+                input.up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
+                input.down = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+                input.left = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+                input.right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+            }
+        );
+
+        inputSystem(registry);
+        movementSystem(registry, dt);
+        
+
         window.clear();
+
+        renderSystem(registry, window);
+
         window.display();
 
         while (const std::optional event = window.pollEvent())
